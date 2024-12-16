70e75b46085ce8ae9227ea824a8e5d627e0cda59
GabBraga <gabriel.lima21031@gmail.com>
2024-12-16 20:33:07 -0300

    Criação da pasta do projeto
    Criação do Projeto dentro da pasta com nome de quem fez

---
+++ App_Notas/app_notas_braga/README.md
@@ -0,0 +1,3 @@
+# app_notas_braga
+
+A new Flutter project.
---
+++ App_Notas/app_notas_braga/android/app/src/main/kotlin/com/example/app_notas_braga/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_notas_braga
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
