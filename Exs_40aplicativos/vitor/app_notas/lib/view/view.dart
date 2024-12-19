e665020ea0f0afd820340f05ea02670551f99062
vitorgg1208 <vitorgg1208bug@gmail.com>
2024-12-19 18:53:06 -0300

    atualizações e finalização e começo das melhorias

--- Exs_40aplicativos/vitor/app_notas/lib/view/view.dart
+++ Exs_40aplicativos/vitor/app_notas/lib/view/view.dart
@@ -1,3 +1,4 @@
+import 'package:app_notas/design/thema.dart';
 import 'package:flutter/material.dart';
 
 class FirstPage extends StatelessWidget {
@@ -10,16 +11,33 @@ class FirstPage extends StatelessWidget {
       children: [
         Expanded(
           child: Container(
-            margin: const EdgeInsets.all(20),
-            height:200,
+            margin:
+                const EdgeInsets.only(right: 20, left: 20, top: 20, bottom: 0),
+            height: 150,
             decoration: BoxDecoration(
               color: Colors.blueGrey[50],
               borderRadius: BorderRadius.circular(5),
-              image: DecorationImage(image: AssetImage("img/linhas.avif"))
             ),
-            child: Image.asset("img/linhas.avif"),
+            child: const Column(children: [LinhasDeTexto()]),
           ),
-        )
+        ),
+        const SizedBox(height: 5),
+        Row(
+          children: [
+            Expanded(
+                child: Container(
+              margin: const EdgeInsets.all(20),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5)))),
+                child: const Text("salvar"),
+              ),
+            )),
+          ],
+        ),
+        const SizedBox(height: 20)
       ],
     );
   }
